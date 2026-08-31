27f6bec1661ad6223e6d3e64373404d160671c9f
Anastasiia <anastasiaosega@gmail.com>
2026-08-31 18:04:55 +0300

    feat(models): Add project and task models

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,36 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Priority(models.IntegerChoices):
+    LOW = 1, "Low"
+    MEDIUM = 2, "Medium"
+    HIGH = 3, "High"
+    URGENT = 4, "Urgent"
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="projects",
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    is_completed = models.BooleanField(default=False)
+    deadline = models.DateField(null=True, blank=True)
+    priority = models.IntegerField(choices=Priority, default=Priority.LOW)
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+        related_name="tasks",
+    )
+
+    def __str__(self):
+        return self.name
